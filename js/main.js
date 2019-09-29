353e616620766071cc548f98242b8355def204c7
Yana Stepanova / mansuedei <mansuedei@yandex.ru>
2019-09-30 01:44:00 +0300

    Added overlay on (un)successful order

--- js/main.js
+++ js/main.js
@@ -69,5 +69,97 @@ function loop(direction) {
 //slider end
 
 //form start
+const myForm = document.querySelector('#myForm');
+const sendButton = document.querySelector('#sendButton');
+
+sendButton.addEventListener('click', function(event) {
+	event.preventDefault();
+
+	if (validateForm(myForm)) {
+		let overlay;
+		const URL = 'https://webdev-api.loftschool.com/sendmail';
+		const URL_FAIL = 'https://webdev-api.loftschool.com/sendmail/fail';
+		const data = {
+			name: myForm.elements.name.value,
+			phone: myForm.elements.phone.value,
+			comment: myForm.elements.comment.value,
+			to: myForm.elements.to.value,
+		};
+
+		const xhr = new XMLHttpRequest();
+		xhr.responseType = 'json';
+
+		xhr.open('POST', URL);
+		xhr.send(new FormData(myForm));
+		xhr.addEventListener('load', res => {
+			if (xhr.response.status) {
+				overlay = createOverlay(
+					'Ваш заказ принят. Спасибо, что выбрали CHOCCO!'
+				);
+			} else {
+				overlay = createOverlay(
+					'К сожалению, при отправке заказа произошла ошибка. Пожалуйста, попробуйте еще раз. Спасибо!'
+				);
+			}
+
+			document.body.appendChild(overlay);
+		});
+	}
+});
+
+function validateForm(form) {
+	let valid = true;
+
+	if (!validateField(form.elements.name)) {
+		valid = false;
+	}
+	if (!validateField(form.elements.phone)) {
+		valid = false;
+	}
+
+	if (!validateField(form.elements.comment)) {
+		valid = false;
+	}
+
+	if (!validateField(form.elements.to)) {
+		valid = false;
+	}
+
+	return valid;
+}
+
+function validateField(field) {
+	if (!field.checkValidity()) {
+		field.nextElementSibling.textContent = field.validationMessage;
+
+		return false;
+	} else {
+		field.nextElementSibling.textContent = '';
+
+		return true;
+	}
+}
 
 //form end
+
+//overlay start
+
+function createOverlay(content) {
+	const overlayElement = document.createElement('div');
+	overlayElement.classList.add('overlay');
+
+	const template = document.querySelector('#formOverlay');
+	overlayElement.innerHTML = template.innerHTML;
+
+	const closeElement = overlayElement.querySelector('.overlay__close');
+	closeElement.addEventListener('click', function() {
+		document.body.removeChild(overlayElement);
+	});
+
+	const contentElement = overlayElement.querySelector('.overlay__content');
+	contentElement.innerHTML = content;
+
+	return overlayElement;
+}
+
+//overlay window end
